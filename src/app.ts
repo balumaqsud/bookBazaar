a0af4d3c64191d69502e95e0b2d65dded80ccfbc
balumaqsud <ollobergan008@mail.ru>
2025-03-16 10:36:58 +0900

    path added

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import express from "express"
+import path from "path"
 
 //enterance ---middlewares
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
 //sessions
 //views
-app.set("views", "views");
+app.set("views", path.join(__dirname, "public"));
 app.set("view engine", "ejs");
 
 //routnig
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose"
-import path from "path"
 import dotenv from "dotenv"
 import app from "./app"
 dotenv.config()
