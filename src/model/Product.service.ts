72756ef1d4a80474de7b4f43ec5e027755353e9b
balumaqsud <ollobergan008@mail.ru>
2025-03-30 17:48:34 +0900

    fix: product.service updated

--- src/model/Product.service.ts
+++ src/model/Product.service.ts
@@ -1,3 +1,10 @@
-class ProductService {}
+import ProductSchemaModel from "../schema/Product.model";
+class ProductService {
+  private readonly productModel;
+
+  constructor() {
+    this.productModel = ProductSchemaModel;
+  }
+}
 
 export default ProductService;
